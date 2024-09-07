86030dfa03e848197510039b8578bc99644ce505
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-08 01:17:32 +0500

    Que 19: Dinner Guests

---
+++ Main.ts
@@ -0,0 +1,45 @@
+// Que 19: Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the
+// number of people you are inviting to dinner.   
+
+// ********************************************************************************************************************************
+
+// Working with one of the programs from Exercises 14 through 18
+
+let guestList: string[] = ["Zara" , "Sheza" , "Laiba"];
+let absentGuest: string = "Zara";
+
+let newGuest: string = "Hooria";
+guestList[0] = newGuest;
+
+// • Add one new guest to the beginning of your array.
+
+guestList.unshift("Kiran");
+
+// • Add one new guest to the middle of your array. 
+
+guestList.splice(2 , 0 , "Areeba");
+
+// • Use append() to add one new guest to the end of your list.
+
+guestList.push("Anusha");
+
+
+// • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from
+//   your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+
+while(guestList.length > 2) {
+   let removeGuest = guestList.pop();
+//    console.log(`Sorry ${removeGuest}, You are not invited for dinner.\n`);
+};
+
+// • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have 
+//   an empty list at the end of your program.
+
+guestList.splice(0 , 2);
+console.log(guestList);
+
+// ======================================= Exercise 19
+
+// Print a message indicating the number of people you are inviting to dinner. 
+
+console.log(`Total number of Guest Are: ${guestList.length}`);
